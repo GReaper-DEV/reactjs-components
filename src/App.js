a797eb6198403394429de2fb1985a2e6b80b71f6
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-07 10:17:39 +0200

    Added tailwind classses, classnames package and twMerge.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Button from './components/Button'
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
+
 
 function App(){
-    return <div>App</div>;
+
+
+    const handleClick = () => {
+        console.log('Clicked me!')
+    }
+
+    return <div>
+        <div><Button onClick={handleClick} primary rounded outline className="mb-5"><GoBell /> Hi there!</Button></div>
+        <div><Button secondary rounded outline><GoCloudDownload /> Hi there!</Button></div>
+        <div><Button success rounded outline><GoDatabase /> Hi there!</Button></div>
+        <div><Button warning rounded outline> Hi there!</Button></div>
+        <div><Button danger rounded outline>Hi there!</Button></div>
+    </div>;
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {twMerge} from 'tailwind-merge';
+import classNames from 'classnames';
+
+function Button({
+                    children,
+                    text,
+                    primary,
+                    secondary,
+                    success,
+                    warning,
+                    danger,
+                    outline,
+                    rounded,
+                    ...rest
+                }) {
+
+    const classes = twMerge(
+        classNames(rest.className,'px-3 py-1.5 border flex items-center gap-2', {
+            'border-blue-500 bg-blue-500 text-white hover:border-blue-500 hover:bg-white hover:text-blue-500 transition': primary,
+            'border-gray-900 bg-gray-900 text-white hover:border-gray-500 hover:bg-white hover:text-gray-500 transition': secondary,
+            'border-green-500 bg-green-500 text-white hover:border-green-500 hover:bg-white hover:text-green-500 transition': success,
+            'border-yellow-400 bg-yellow-400 text-white hover:border-yellow-500 hover:bg-white hover:text-yellow-500 transition': warning,
+            'border-red-500 bg-red-500 text-white hover:border-red-500 hover:bg-white hover:text-red-500 transition': danger,
+            'rounded-full': rounded,
+            'bg-white': outline,
+            'text-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white transition': outline && primary,
+            'text-gray-900 hover:border-gray-500 hover:bg-gray-500 hover:text-white transition': outline && secondary,
+            'text-green-500 hover:border-green-500 hover:bg-green-500 hover:text-white transition': outline && success,
+            'text-yellow-400 hover:border-yellow-500 hover:bg-yellow-500 hover:text-white transition': outline && warning,
+            'text-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white transition': outline && danger
+        })
+    );
+
+    Button.propTypes = {
+        checkVariationValue: ({primany, secondary, success, warning, danger}) => {
+            const count =
+                Number(!!primany) +
+                Number(!!secondary) +
+                Number(!!success) +
+                Number(!!warning) +
+                Number(!!danger);
+            if (count > 1) {
+                throw Error('Button Component -> Please choose only one value between primany, secondary, success, warning, danger.')
+            }
+        },
+        outline: PropTypes.bool,
+        rounded: PropTypes.bool,
+    }
+
+    return <button {...rest} className={classes}>{children}</button>
+}
+
+export default Button;
