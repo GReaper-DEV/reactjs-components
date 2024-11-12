6530644df61380c271adb3c9108f2b5fddba4afc
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-12 12:26:17 +0200

    ButtonPage is a page that has the button component.

---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,21 @@
+import Button from '../components/Button'
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
+
+
+function ButtonPage(){
+
+
+    const handleClick = () => {
+        console.log('Clicked me!')
+    }
+
+    return <div>
+        <div><Button onClick={handleClick} primary rounded outline className="mb-5"><GoBell /> Hi there!</Button></div>
+        <div><Button secondary rounded outline><GoCloudDownload /> Hi there!</Button></div>
+        <div><Button success rounded outline><GoDatabase /> Hi there!</Button></div>
+        <div><Button warning rounded outline> Hi there!</Button></div>
+        <div><Button danger rounded outline>Hi there!</Button></div>
+    </div>;
+}
+
+export default ButtonPage;
