c465783b7ae5eab75b117bc2239409abbb749ac7
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-12 12:27:11 +0200

    Added dropdown component.

---
+++ src/components/Dropdown.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+function Dropdown({options}){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('Select...');
+
+    const handleClick = () => {
+        setIsOpen((currentIsOpen) => !isOpen);
+    }
+
+    const handleOptionClick = (option) => {
+        setIsOpen(false);
+        setSelectedOption(option.label);
+    }
+
+    const renderedOptions = options.map((option) => {
+        return <div onClick={() => handleOptionClick(option)} key={option.value}>
+            {option.label}
+        </div>;
+    })
+
+    return (
+        <div>
+            <div onClick={handleClick}>{selectedOption}</div>
+            {isOpen && <div>{renderedOptions}</div>}
+        </div>
+    );
+}
+
+export default Dropdown;
