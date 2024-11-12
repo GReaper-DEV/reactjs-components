9574ecd970e5490dea4ec7a53295c3cdc112adcb
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-12 12:25:59 +0200

    AccordionPage is a page that has the accordion component.

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,30 @@
+import ButtonPage from "./ButtonPage"
+import Accordion from "../components/Accordion"
+
+function AccordionPage(){
+
+    const items = [
+        {
+            id: 'akjsdh',
+            label: 'Click here',
+            content: 'Here is more info'
+        },
+        {
+            id: 'akjsdh4',
+            label: 'Click here 2',
+            content: 'Here is more info'
+        },
+        {
+            id: 'akjsdhs',
+            label: 'Click here 3',
+            content: 'Here is more info'
+        },
+    ]
+
+    return <>
+        <ButtonPage />
+        <Accordion items={items} />
+    </>
+}
+
+export default AccordionPage;
