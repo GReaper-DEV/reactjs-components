e379139d403a1410e2e5dd8df702a6f885af3546
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-12 12:27:30 +0200

    Just updated to the needs of the app.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Button from './components/Button'
-import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
+import Dropdown from "./components/Dropdown.js"
+function App() {
 
+    const options = [
+        {
+            label: 'Red',
+            value: 'red',
+        },
+        {
+            label: 'Green',
+            value: 'green',
+        },
+        {
+            label: 'Blue',
+            value: 'blue',
+        },
+    ];
 
-function App(){
-
-
-    const handleClick = () => {
-        console.log('Clicked me!')
-    }
-
-    return <div>
-        <div><Button onClick={handleClick} primary rounded outline className="mb-5"><GoBell /> Hi there!</Button></div>
-        <div><Button secondary rounded outline><GoCloudDownload /> Hi there!</Button></div>
-        <div><Button success rounded outline><GoDatabase /> Hi there!</Button></div>
-        <div><Button warning rounded outline> Hi there!</Button></div>
-        <div><Button danger rounded outline>Hi there!</Button></div>
-    </div>;
+    return <Dropdown options={options} />
 }
 
 export default App;
