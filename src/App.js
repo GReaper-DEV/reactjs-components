e2bcb54ef1f236d757c5311ca44ded2fa97c2a23
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-13 13:57:33 +0200

    Updated to have Route components and Link components.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Dropdown from "./components/Dropdown.js"
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+
 function App() {
+    return <div>
+        <Link to="/accordion">Go to accordion</Link>
+        <Link to="/dropdown">Go to dropdown</Link>
+        <div>
+            <Route path="/accordion">
+                <AccordionPage />
+            </Route>
+            <Route path="/dropdown">
+                <DropdownPage />
+            </Route>
+        </div>
+    </div>
 
-    const options = [
-        {
-            label: 'Red',
-            value: 'red',
-        },
-        {
-            label: 'Green',
-            value: 'green',
-        },
-        {
-            label: 'Blue',
-            value: 'blue',
-        },
-    ];
-
-    return <Dropdown options={options} />
 }
 
 export default App;
