e9688791cde45b6051172eb2cadd040b1c06c22a
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2024-11-13 13:54:03 +0200

    Added navigation context and provider.

---
+++ src/context/navigation.js
@@ -0,0 +1,32 @@
+import {createContext, useEffect, useState} from 'react';
+
+
+
+const NavigationContext = createContext();
+
+function NavigationProvider({children}){
+
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        }
+
+        document.addEventListener("popstate", handler);
+
+        return () => document.removeEventListener("popstate", handler);
+
+    }, []);
+
+    const navigate = (to) => {
+        window.history.pushState({}, '', to);
+        setCurrentPath(window.location.pathname);
+    }
+    return <NavigationContext.Provider value={{currentPath, navigate}}>
+        {children}
+    </NavigationContext.Provider>
+}
+
+export default NavigationProvider;
+export {NavigationContext};
